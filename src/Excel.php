32c1fb53cb88b102504613ab6749cda08264d159
DevNull-IR <97373457+DevNull-IR@users.noreply.github.com>
2023-03-27 22:32:13 +0330

    fixing

--- src/Excel.php
+++ src/Excel.php
@@ -4,12 +4,25 @@ namespace ExcelDriver;
 
 use Facades\Facade;
 
+
+/**
+ * @method static array|bool openDocument(string $file_path, bool $isNullCreated = true)
+ *
+ * @method static bool editDocument(string $file_path, array $data = [], bool $isNullCreated = false)
+ *
+ * @method static bool editWithClear(string $file_path, array $data = [], bool $isNullCreated = false)
+ *
+ * @method static bool createDocument(string $file_path, array $data)
+ *
+ * @method static getWithCompiler(string $file_path)
+ * @method static buildWithCompiler(string $file_path, array $data)
+ */
 class Excel extends Facade
 {
     /**
      * @return string
      */
-    public static function setNameSpace()
+    public static function setNameSpace(): string
     {
         return ExcelService::class;
     }
--- src/ExcelService.php
+++ src/ExcelService.php
@@ -13,8 +13,9 @@ class ExcelService
     private function check($file_path, $isNullCreated): bool
     {
         $somNext = false;
-        if (! file_exists($file_path)){
+        if (! file_exists($file_path)){echo "ab";
             if ($isNullCreated){
+
                 $somNext = true;
                 file_put_contents($file_path, NULL);
             }
@@ -32,20 +33,39 @@ class ExcelService
         return $somNext;
     }
 
+
     /**
      * @param string $file_path
      * @param bool $isNullCreated
      * @return array|bool
      */
-    public function openDocument(string $file_path, bool $isNullCreated = true): array|bool
+    public function openDocument(string $file_path, bool $isNullCreated = true, bool $compailer = false): array|bool
     {
         if ($this->check($file_path, $isNullCreated)){
             $file = file_get_contents($file_path);
             $exp = explode(PHP_EOL, $file);
             $implode = [];
-            foreach ($exp as $item) {
-                $getArray = explode(",", $item);
-                $implode[] = $getArray;
+            foreach ($exp as $key => $item) {
+                if (! $compailer) {
+                    $getArray = explode(",", $item);
+                    $implode[] = $getArray;
+                }else{
+                    $getArray = explode(",=", "," . $item);
+                    $i = 0;
+                    foreach ($getArray as $keyA => $itemArray) {
+                        if (is_null($itemArray) or $itemArray == ""){
+                            if ($i++ != 0){
+                                $implode[$key][] = "";
+                            }
+                            continue;
+                        }
+                        if (!str_starts_with($itemArray, ",")){
+                            $implode[$key][] = eval("return $itemArray;");
+                        }else{
+                            $implode[$key] = explode(",", substr($itemArray, 1, strlen($itemArray)));
+                        }
+                    }
+                }
             }
             return $implode;
         }
@@ -99,4 +119,48 @@ class ExcelService
 
         return false;
     }
+
+    /**
+     * @param string $file_path
+     * @param array $data
+     * @return bool
+     */
+    public function createDocument(string $file_path, array $data): bool
+    {
+        return $this->editWithClear($file_path, $data, true);
+    }
+
+    /**
+     * @param string $file_path
+     * @return array|false
+     */
+    public function getWithCompiler(string $file_path)
+    {
+        if ($this->check($file_path, false)){
+            $get = $this->openDocument($file_path, false, true);
+
+            return $get;
+        }
+        return false;
+    }
+
+
+    /**
+     * @param string $file_path
+     * @param array $data
+     * @return bool
+     */
+
+    public function buildWithCompiler(string $file_path, array $data)
+    {
+        $this->createDocument("TMP.csv", $data);
+        $get = $this->editWithClear(
+            file_path: $file_path,
+            data: $this->getWithCompiler("TMP.csv"),
+            isNullCreated: true);
+        unlink("TMP.csv");
+
+        return $get;
+    }
+
 }
